06cec95414016a397f74cb922e90edb1c6d00e86
Marco Antonio Gil <marco.gil@multisafepay.com>
2026-03-03 15:23:33 +0100

    PTHMINT-94: Add OSLv3 license headers to tests
    Add copyright, Open Software License (OSL) v3.0 and disclaimer header comments to several test and support modules for legal/attribution purposes. Affected files: tests/multisafepay/e2e/examples/transport/test_custom_httpx_transport.py, tests/multisafepay/e2e/examples/transport/test_custom_requests_session_transport.py, tests/multisafepay/e2e/examples/transport/test_custom_urllib3_transport.py, tests/multisafepay/unit/transport/test_unit_transport_selection.py, tests/support/alt_http_transports.py, tests/support/mock_transport.py. No functional code changes.

--- tests/multisafepay/e2e/examples/transport/test_custom_requests_session_transport.py
+++ tests/multisafepay/e2e/examples/transport/test_custom_requests_session_transport.py
@@ -1,3 +1,10 @@
+# Copyright (c) MultiSafepay, Inc. All rights reserved.
+
+# This file is licensed under the Open Software License (OSL) version 3.0.
+# For a copy of the license, see the LICENSE.txt file in the project root.
+
+# See the DISCLAIMER.md file for disclaimer details.
+
 """
 Test module for e2e testing.
 
